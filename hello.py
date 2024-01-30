19452beb21af66bd54f4360eed2614e8e19d0c8c
gheacs <gheacs@uw.edu>
2024-01-30 15:43:31 -0800

    finalize visualization

--- hello.py
+++ hello.py
@@ -1,29 +1,45 @@
 import streamlit as st
 import datetime
 from zoneinfo import ZoneInfo
+import time
 
 # Create streamlit app
 st.title("World Clock")
-st.write("This app shows the current time in different time zones.")
+st.write("This app shows the current time and date in different time zones.")
 
-# Get date and current time
-Seattle_current_time = datetime.datetime.now(ZoneInfo("America/Los_Angeles"))
-Indonesia_current_time = datetime.datetime.now(ZoneInfo("Asia/Jakarta"))
-New_York_current_time = datetime.datetime.now(ZoneInfo("America/New_York"))
+# Get Current Time
+def get_current_time_and_date(timezone):
+    current_time = datetime.datetime.now(ZoneInfo(timezone))
+    return current_time.strftime("%Y-%m-%d %H:%M:%S")
 
 
-# Visualize the time in different time zones
-st.write("Seattle")
-st.write("Indonesia")
-st.write("New York")
+Seattle_current_time = get_current_time_and_date("America/Los_Angeles")
+Indonesia_current_time = get_current_time_and_date("Asia/Jakarta")
+New_York_current_time = get_current_time_and_date("America/New_York")
 
-# Create a clock for each time zone
-st.write(Seattle_current_time.strftime("%H:%M:%S"))
-st.write(Indonesia_current_time.strftime("%H:%M:%S"))
-st.write(New_York_current_time.strftime("%H:%M:%S"))
+# Visualize
+st.subheader("Seattle")
+st.image("Seattle.jpg", width=200)
+seattle_time_display = st.text(Seattle_current_time)
 
-# Add images
-st.image("https://www.pngkey.com/png/full/114-1149878_seattle-skyline-seattle-skyline-silhouette-png.png", width=300)
-st.image("https://www.pngkey.com/png/full/114-1149878_seattle-skyline-seattle-skyline-silhouette-png.png", width=300)
-st.image("https://www.pngkey.com/png/full/114-1149878_seattle-skyline-seattle-skyline-silhouette-png.png", width=300)
+st.subheader("Indonesia")
+st.image("Jakarta.jpeg", width=200)
+indonesia_time_display = st.text(Indonesia_current_time)
 
+st.subheader("New York")
+st.image("new york.jpg", width=200)
+new_york_time_display = st.text(New_York_current_time)
+
+# set refresh interval
+refresh_interval = 30  # 30 minutes
+
+while True:
+    time.sleep(refresh_interval * 60)  # Sleep for 30 minutes
+    # Update the time and date displays with the current time and date
+    Seattle_current_time = get_current_time_and_date("America/Los_Angeles")
+    Indonesia_current_time = get_current_time_and_date("Asia/Jakarta")
+    New_York_current_time = get_current_time_and_date("America/New_York")
+    
+    seattle_time_display.text(Seattle_current_time)
+    indonesia_time_display.text(Indonesia_current_time)
+    new_york_time_display.text(New_York_current_time)
