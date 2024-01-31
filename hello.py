be8fbed3d54f3e9e9202cb6dbcd604444f4b5f77
gheacs <gheacs@uw.edu>
2024-01-30 16:08:55 -0800

    add dropdown for timezone

--- hello.py
+++ hello.py
@@ -17,29 +17,23 @@ Seattle_current_time = get_current_time_and_date("America/Los_Angeles")
 Indonesia_current_time = get_current_time_and_date("Asia/Jakarta")
 New_York_current_time = get_current_time_and_date("America/New_York")
 
-# Visualize
-st.subheader("Seattle")
-st.image("Seattle.jpg", width=200)
-seattle_time_display = st.text(Seattle_current_time)
+#create dropdown
+timezone = st.selectbox("Select Timezone", ["America/Los_Angeles", "Asia/Jakarta", "America/New_York"])
 
-st.subheader("Indonesia")
-st.image("Jakarta.jpeg", width=200)
-indonesia_time_display = st.text(Indonesia_current_time)
+# filter the timezones based on the selected timezone
+if timezone == "America/Los_Angeles":
+        current_time = get_current_time_and_date("America/Los_Angeles")
+        st.subheader("Seattle / Los Angeles")
+        st.image("Seattle.jpg", width=200)
+        seattle_time_display = st.text(Seattle_current_time)
+elif timezone == "Asia/Jakarta":
+        current_time = get_current_time_and_date("Asia/Jakarta")
+        st.subheader("Indonesia")
+        st.image("Jakarta.jpeg", width=200)
+        indonesia_time_display = st.text(Indonesia_current_time)    
+elif timezone == "America/New_York":
+        current_time = get_current_time_and_date("America/New_York")
+        st.subheader("New York")
+        st.image("new york.jpg", width=200)
+        new_york_time_display = st.text(New_York_current_time)  
 
-st.subheader("New York")
-st.image("new york.jpg", width=200)
-new_york_time_display = st.text(New_York_current_time)
-
-# set refresh interval
-refresh_interval = 30  # 30 minutes
-
-while True:
-    time.sleep(refresh_interval * 60)  # Sleep for 30 minutes
-    # Update the time and date displays with the current time and date
-    Seattle_current_time = get_current_time_and_date("America/Los_Angeles")
-    Indonesia_current_time = get_current_time_and_date("Asia/Jakarta")
-    New_York_current_time = get_current_time_and_date("America/New_York")
-    
-    seattle_time_display.text(Seattle_current_time)
-    indonesia_time_display.text(Indonesia_current_time)
-    new_york_time_display.text(New_York_current_time)
