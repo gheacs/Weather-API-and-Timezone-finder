3944b517e457b55f18f0513b175791f117f699cb
gheacs <gheacs@uw.edu>
2024-02-08 09:35:33 -0800

    revise app.py

--- app.py
+++ app.py
@@ -20,20 +20,24 @@ New_York_current_time = get_current_time_and_date("America/New_York")
 #create dropdown
 timezone = st.selectbox("Select Timezone", ["America/Los_Angeles", "Asia/Jakarta", "America/New_York"])
 
+
+
 # filter the timezones based on the selected timezone
 if timezone == "America/Los_Angeles":
         current_time = get_current_time_and_date("America/Los_Angeles")
         st.subheader("Seattle / Los Angeles")
         st.image("Seattle.jpg", width=200)
-        seattle_time_display = st.text(Seattle_current_time)
+        
 elif timezone == "Asia/Jakarta":
         current_time = get_current_time_and_date("Asia/Jakarta")
         st.subheader("Indonesia")
         st.image("Jakarta.jpeg", width=200)
-        indonesia_time_display = st.text(Indonesia_current_time)    
+        
 elif timezone == "America/New_York":
         current_time = get_current_time_and_date("America/New_York")
         st.subheader("New York")
         st.image("new york.jpg", width=200)
-        new_york_time_display = st.text(New_York_current_time)  
 
+time_display = st.empty() # create a placeholder for the time
+time_display.text(f"The current time in {timezone} is {current_time}")        
+time.sleep(1) # updated every 1 second
